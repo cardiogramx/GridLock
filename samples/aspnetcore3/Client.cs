6a33d8e44b802cac81a2ff8ac33670d3920116eb
cardiogram <felix@abi-ng.com>
2019-11-03 23:31:38 +0100

    added a custom client class to demonstrate custom class usage of GridLock

---
+++ samples/aspnetcore3/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using GridLock;
+
+namespace aspnetcore3
+{
+    public class Client : GridLockItem
+    {
+        public DateTime DateTimeAdded { get; set; } = DateTime.UtcNow;
+
+        public bool IsActive { get; set; } = true;
+    }
+}
--- samples/aspnetcore3/Controllers/WeatherForecastController.cs
+++ samples/aspnetcore3/Controllers/WeatherForecastController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading;
 
 using GridLock.AspNetCore.Mvc.Authorization;
+using GridLock;
 
 namespace aspnetcore3.Controllers
 {
@@ -13,31 +15,29 @@ namespace aspnetcore3.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
+        private static readonly string[] Summaries = {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IGridLock gridLock;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IGridLock gridLock)
         {
             _logger = logger;
+            this.gridLock = gridLock;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+
+        [HttpGet("Add/{id}/{level}")]
+        public async Task<GridLockItem> Add(string Id, int? level, CancellationToken cancellation)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return level switch
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                null => await gridLock.AddAsync(new Client { Id = Id, Level = 1 }),
+                _ => await gridLock.AddAsync(new Client { Id = Id, Level = level.Value }, cancellation)
+            };
         }
-
 
         [HttpGet("Test")]
         [Locked(AuthorizedLevels = new int[]{ 1 })]
@@ -52,5 +52,29 @@ namespace aspnetcore3.Controllers
             })
             .ToArray();
         }
+
+
+        [HttpGet("List")]
+        public async Task<List<GridLockItem>> List(CancellationToken cancellation)
+        {
+            var response = await gridLock.ListAsync(cancellation);
+
+            return response;
+        }
+
+
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
     }
 }
