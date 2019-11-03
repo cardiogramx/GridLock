f7ac80fb3e3aac2b8a91f1faddedc7ef801bbeb0
cardiogram <felix@abi-ng.com>
2019-11-03 23:46:25 +0100

    added an extension to convert between custom data and GridLockItem

---
+++ src/GridLockExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GridLock
+{
+    public static class GridLockExtensions
+    {
+        public static T ToObject<T>(this GridLockItem gridLockItem) where T : GridLockItem
+        {
+            return gridLockItem as T;
+        }
+    }
+}
