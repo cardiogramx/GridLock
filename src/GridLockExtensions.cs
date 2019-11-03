167603c6e61198025e1e73eee1441a59464829c2
cardiogram <felix@abi-ng.com>
2019-11-04 00:00:22 +0100

    added an extension to convert list of GridLockItem to a list of custom objects

--- src/GridLockExtensions.cs
+++ src/GridLockExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace GridLock
 {
     public static class GridLockExtensions
@@ -6,6 +8,11 @@ namespace GridLock
         public static T ToObject<T>(this GridLockItem gridLockItem) where T : GridLockItem
         {
             return gridLockItem as T;
+        }
+
+        public static T ToObjects<T>(this List<GridLockItem> gridLockItems) where T : List<GridLockItem>
+        {
+            return gridLockItems as T;
         }
     }
 }
