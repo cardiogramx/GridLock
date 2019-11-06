74bb3089637e77ce5a0e2331c8a7618af0bdb41a
cardiogram <felix@abi-ng.com>
2019-11-06 11:45:27 +0100

    added GridLock middleware extension

---
+++ src/GridLockMiddlewareExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace GridLock
+{
+    public static class GridLockMiddlewareExtension
+    {
+        public static void UseGridLock(this IApplicationBuilder app, IGridLock gridLock)
+        {
+            app.Use(async (context, next) =>
+            {
+                var key = context.Request.Headers["Authorization"].ToString();
+
+                if (string.IsNullOrWhiteSpace(key) || await gridLock.ValidateAsync<GridLockItem>(key) == false)
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync(string.Empty);
+                }
+
+                await next.Invoke();
+
+            });
+
+
+        }
+    }
+}
